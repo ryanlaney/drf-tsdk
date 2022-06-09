274a454bfcf804b06e545b61debd31227a23448c
ryanlaney <ryanlaney@gmail.com>
2022-06-09 15:09:30 -0400

    bump

--- setup.py
+++ setup.py
@@ -1,30 +1,35 @@
-import setuptools
+from setuptools import setup
 
-with open('README.md') as file:
-    long_description = file.read()
+name = "drf-tsdk"
+package = "drf_tsdk"
+version = "0.1.0"
+description = "A package to generate a TypeScript API client for Django Rest Framework views and viewsets."
+url = "https://github.com/ryan.laney/drf-tsdk"
+author = "Ryan Laney"
+author_email = "ryanlaney@gmail.com"
 
-setuptools.setup(
-    name="drf-tsdk",
-    version="0.1.0",
-    url="https://github.com/ryan.laney/drf-tsdk",
+with open("README.md", "utf8") as readme:
+    long_description = readme.read()
 
-    author="Ryan Laney",
-    author_email="ryanlaney@gmail.com",
+with open("requirements.txt", "utf8") as fh:
+    requirements = [
+        r.strip() for r in fh.read().split("\n") if not r.strip().startswith("#")
+    ]
 
-    description="A package to generate a TypeScript API client for Django Rest Framework views and viewsets.",
+setup(
+    name=name,
+    version=version,
+    url=url,
+    author=author,
+    author_email=author_email,
+    description=description,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords=['Django', 'Django Rest Framework',
-              'DRF', 'Typescript', 'Python', 'API'],
-
-    packages=['drf_tsdk'],
+    long_description_content_type="text/markdown",
+    keywords=["Django", "Django Rest Framework", "DRF", "Typescript", "Python", "API"],
+    packages=[package],
     include_package_data=True,
     zip_safe=False,
-    platforms='any',
-
-    install_requires=[
-        'django',
-        'djangorestframework',
-        'jsbeautifier'
-    ]
+    platforms="any",
+    python_requies=">=3.6",
+    install_requires=requirements,
 )
